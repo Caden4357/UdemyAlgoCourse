c4290e1b0eb6646f14ba6e4d191b96ad68ca636f
Caden Wilcox <wilcox4357@gmail.com>
2025-05-22 10:40:51 -0700

    reverse SLL

---
+++ Lists/DLL/dll.js
@@ -0,0 +1,134 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        const newNode = new Node(val);
+        // check if the list is empty
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+            this.length++;
+        }
+        else {
+            // if not, the tail should point to the new node
+            this.tail.next = newNode;
+            // the new node's previous should point to the tail
+            newNode.prev = this.tail;
+            // the tail should now be the new node
+            this.tail = newNode;
+
+            this.length++; // increment the length
+        }
+        // return the list
+        return this;
+    }
+    pop(){
+        // if htere is no head return undefined
+        if(!this.head) return undefined;
+        // store the current tail in a variable to return later
+        const poppedNode = this.tail;
+        // if the length is 1, set the head and tail to be null
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else {
+            // update the tail to be the previous Node
+            this.tail = poppedNode.prev;
+            // set the new tail's next to null
+            this.tail.next = null;
+            // cut the old tail's previous to null
+            poppedNode.prev = null;
+        }
+        this.length--;
+        return poppedNode;
+    }
+    shift(){
+        // if there is no head return undefined
+        if(!this.head) return undefined;
+        // store the current head in a variable to return later
+        const oldHead = this.head;
+        // if the length is 1, set the head and tail to be null
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else {
+            // update the head to be the next Node
+            this.head = oldHead.next;
+            // set the new head's previous to null
+            this.head.prev = null;
+            // cut the old head's next to null
+            oldHead.next = null;
+        }
+        // decrement the length
+        this.length--;
+        // return the old head
+        return oldHead;
+    }
+    unshift(val){
+        // create a new node with the value passed to the function
+        const newNode = new Node(val);
+        // if the length is 0, set the head and tail to be the new node
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            // otherwise set the new node's next to the current head
+            newNode.next = this.head;
+            // set the current head's previous to the new node
+            this.head.prev = newNode;
+            // set the head to the new node
+            this.head = newNode;
+        }
+        // increment the length
+        this.length++;
+        // return the list
+        return this;
+    }
+    get(index){
+        // if the index is less than 0 or greater or equal to the length, return null
+        if(index < 0 || index >= this.length) return null;
+        let current = index <= this.length/2 ? this.head : this.tail; // create a variable to store the node at the specified index
+        // if the index is less than or equal to half the length of the list
+        if(index <= this.length/2){
+            let count = 0;  // create a count variable
+            // loop through the list starting from the head while the count is not equal to the index
+            while(count !== index){
+                current = current.next;
+                count++;
+            }
+        } else {
+            let count = this.length - 1;
+            while(count !== index){
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }   
+}
+dll = new DoublyLinkedList();
+dll.push(1);
+dll.push(2);    
+dll.push(3);
+
+console.log(dll.get(1));
+// console.log(dll.pop());
+// dll.shift();
+// dll.unshift(0);
+
+
+
+
+
+// console.log(dll);
---
+++ Lists/SLL/sll.js
@@ -0,0 +1,191 @@
+class Node {
+    constructor(data, nextNode = null){
+        this.data=data;
+        this.next=nextNode;
+    }
+}
+class SinglyLinkedList {
+    constructor(startingNode = null) {
+        this.head = startingNode;
+        this.tail = startingNode;
+        this.length = startingNode ? 1 : 0;
+    }
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    pop(){
+        // If the list is empty
+        if(!this.head){
+            return undefined;
+        }
+        else if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+            this.length--;
+            return this;
+        }
+        else{
+            let runner = this.head;
+            while(runner.next.next){
+                runner = runner.next;
+            }
+            runner.next = null;
+            this.tail = runner;
+            this.length--;
+            return this;
+        }
+    }
+    shift(){
+        // if list is empty return undefined
+        if(!this.head){
+            return undefined;
+        }
+        // Take current head save it to var to return it change the head to be the head.next
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return currentHead;
+    }
+    // unshift
+    unshift(val){
+        // create a node using the val passed in 
+        let newNode = new Node(val);
+        // if there is no head set head and tail to newNode
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            // otherwise set the newly created nodes next property to be the current head 
+            newNode.next = this.head;
+            // set the head to newNode
+            this.head = newNode;
+            // increment length 
+            this.length++;
+            return this;
+        }
+    }
+    get(index){
+        if(index < 0 || index > this.length) return null;
+        let i = 0;
+        let current = this.head;
+        while(i !== index){
+            current = current.next;
+            i++;
+        }
+        return current;
+    }
+    set(index, val){
+        let nodeToChange = this.get(index);
+        if(!nodeToChange){
+            return false;
+        }
+        nodeToChange.data = val;
+        return true;
+    }
+    insert(index, val){
+        if(index < 0 || index > this.length) return false;
+        if(index === this.length) return !!this.push(val);
+        if(index === 0) return !!this.unshift(val);
+        else{
+            let newNode = new Node(val);
+            let prev = this.get(index-1);
+            let oldNode = prev.next;
+            prev.next = newNode;
+            newNode.next = oldNode;
+            this.length++;
+            return true;
+        }
+    }
+    remove(index){
+        if(index < 0 || index > this.length) return undefined;
+        if(index === this.length-1) return !!this.pop();
+        if(index === this.length-1) return !!this.shift();
+        let node1 = this.get(index-1);
+        let nodeToBeRemoved = node1.next;
+        node1.next = nodeToBeRemoved.next;
+        this.length--;
+        return nodeToBeRemoved;
+    }   
+    display(){
+        let runner = this.head;
+        while(runner){
+            console.log('RUNNER: ', runner.data, 'NEXT: ', runner.next);
+            runner = runner.next;
+        }
+        console.log(`The length is: ${this.length}`);
+    }
+
+    // reverse list 
+    // 13 - 27 - 32 - 71 
+    // swap head and tail 
+    // create a var called next and prev 
+    // initialize a node to start at the head 
+    // loop through the list 
+    // save the next to be the next on whatever node is 
+    // set the next prop on the node to be whatever prev is 
+    // set prev to be the value of the node variable 
+    // set node var the be the value of next var
+    // 13 - 27 - 32 - 71 
+    //           27 - 13
+    // T
+    // reverse(){
+    //     const oldHead = this.head;
+    //     const oldTail = this.tail;
+    //     this.head = this.tail;
+    //     this.tail = oldHead;
+    //     let current = this.tail;
+    //     while(current){
+    //         // current is 13
+    //         const nextNode = current.next; // 27 
+    //         const nextNodesNext= current.next.next; // 32
+    //         const oldCurrent = current;
+    //         nextNode.next = oldCurrent;
+    //         current = nextNodesNext;
+    //     }
+    //     return this;
+    // }
+    reverse2(){
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        let prev = null;
+        let next;
+        while(node){
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
+}
+let sll = new SinglyLinkedList();
+console.log(sll);
+sll.push(13);
+sll.push(27);
+sll.push(32);
+sll.push(71);
+// // console.log(sll.pop());
+// // console.log(sll.shift());
+// // console.log(sll.shift());
+// // console.log(sll.unshift(0));
+// // sll.set(2, 24)
+// // console.log(sll.get(2));
+// console.log(sll.insert(2, 34));
+// // console.log(sll.get(3));
+// console.log(sll.remove(2));
+console.log(sll.reverse2());
+console.log(sll.display());
