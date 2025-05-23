2e2b4dd298b524351a3a5d4bbf16c4cf99e43787
Caden Wilcox <wilcox4357@gmail.com>
2025-05-23 15:28:11 -0700

    remove and practice converting to TS

--- Lists/DLL/dll.ts
+++ Lists/DLL/dll.ts
@@ -1,4 +1,8 @@
-class Node {
+class CustomNode {
+    val: number;
+    next: CustomNode | null;
+    prev: CustomNode | null;
+
     constructor(val) {
         this.val = val;
         this.next = null;
@@ -7,13 +11,16 @@ class Node {
 }
 
 class DoublyLinkedList {
+    head: CustomNode | null;
+    tail: CustomNode | null;
+    length: number;
     constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
-    push(val) {
-        const newNode = new Node(val);
+    push(val: number) {
+        const newNode = new CustomNode(val);
         // check if the list is empty
         if (this.head === null) {
             this.head = newNode;
@@ -22,7 +29,7 @@ class DoublyLinkedList {
         }
         else {
             // if not, the tail should point to the new node
-            this.tail.next = newNode;
+            if (this.tail) this.tail.next = newNode;
             // the new node's previous should point to the tail
             newNode.prev = this.tail;
             // the tail should now be the new node
@@ -33,40 +40,40 @@ class DoublyLinkedList {
         // return the list
         return this;
     }
-    pop(){
+    pop() {
         // if htere is no head return undefined
-        if(!this.head) return undefined;
+        if (!this.head) return undefined;
         // store the current tail in a variable to return later
         const poppedNode = this.tail;
         // if the length is 1, set the head and tail to be null
-        if(this.length === 1){
+        if (this.length === 1) {
             this.head = null;
             this.tail = null;
         } else {
             // update the tail to be the previous Node
-            this.tail = poppedNode.prev;
-            // set the new tail's next to null
-            this.tail.next = null;
+            if (poppedNode) this.tail = poppedNode.prev;
             // cut the old tail's previous to null
-            poppedNode.prev = null;
+            if (poppedNode) poppedNode.prev = null;
+            // set the new tail's next to null
+            if (this.tail) this.tail.next = null;
         }
         this.length--;
         return poppedNode;
     }
-    shift(){
+    shift() {
         // if there is no head return undefined
-        if(!this.head) return undefined;
+        if (!this.head) return undefined;
         // store the current head in a variable to return later
         const oldHead = this.head;
         // if the length is 1, set the head and tail to be null
-        if(this.length === 1){
+        if (this.length === 1) {
             this.head = null;
             this.tail = null;
         } else {
             // update the head to be the next Node
             this.head = oldHead.next;
             // set the new head's previous to null
-            this.head.prev = null;
+            if (this.head) this.head.prev = null;
             // cut the old head's next to null
             oldHead.next = null;
         }
@@ -75,18 +82,18 @@ class DoublyLinkedList {
         // return the old head
         return oldHead;
     }
-    unshift(val){
+    unshift(val: number) {
         // create a new node with the value passed to the function
-        const newNode = new Node(val);
+        const newNode = new CustomNode(val);
         // if the length is 0, set the head and tail to be the new node
-        if(this.length === 0){
+        if (this.length === 0) {
             this.head = newNode;
             this.tail = newNode;
         } else {
             // otherwise set the new node's next to the current head
             newNode.next = this.head;
             // set the current head's previous to the new node
-            this.head.prev = newNode;
+            if (this.head) this.head.prev = newNode;
             // set the head to the new node
             this.head = newNode;
         }
@@ -95,43 +102,47 @@ class DoublyLinkedList {
         // return the list
         return this;
     }
-    get(index){
-        // if the index is less than 0 or greater or equal to the length, return null
-        if(index < 0 || index >= this.length) return null;
-        let current = index <= this.length/2 ? this.head : this.tail; // create a variable to store the node at the specified index
-        // if the index is less than or equal to half the length of the list
-        if(index <= this.length/2){
-            let count = 0;  // create a count variable
-            // loop through the list starting from the head while the count is not equal to the index
-            while(count !== index){
+    get(index: number): CustomNode | null {
+        if (index < 0 || index >= this.length) return null;
+
+        let current: CustomNode | null;
+        let count: number;
+
+        if (index <= this.length / 2) {
+            current = this.head;
+            count = 0;
+            while (current !== null && count !== index) {
                 current = current.next;
                 count++;
             }
         } else {
-            let count = this.length - 1;
-            while(count !== index){
+            current = this.tail;
+            count = this.length - 1;
+            while (current !== null && count !== index) {
                 current = current.prev;
                 count--;
             }
         }
+
         return current;
     }
-    set(index, value){
-        const newNode = new Node(value);
+
+    set(index, value) {
+        const newNode = new CustomNode(value);
         let node = this.get(index);
-        if(node !== null){
+        if (node !== null) {
             node.val = value;
             return true;
         }
         return false;
     }
-    insert(value, index){
-        if(index < 0 || index > this.length) return false;
-        if(index === 0) return !!this.unshift(value);
-        if(index === this.length) return !!this.push(value);
-        else{
-            let newNode = new Node(value);
-            let beforeNode = this.get(index-1); 
+    insert(value, index) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.unshift(value);
+        if (index === this.length) return !!this.push(value);
+        else {
+            let newNode = new CustomNode(value);
+            let beforeNode = this.get(index - 1);
             let afterNode = beforeNode.next;
             beforeNode.next = newNode;
             newNode.prev = beforeNode;
@@ -141,17 +152,17 @@ class DoublyLinkedList {
             return this;
         }
     }
-    print(){
+    print() {
         let current = this.head;
-        while(current){
+        while (current) {
             console.log(`Current Node: ${current.val}`);
             current = current.next;
         }
     }
-    remove(index){
-        if(index < 0 || index >= this.length) return false;
-        if(index === 0) return !! this.shift();
-        if(index === this.length-1) return !! this.pop();
+    remove(index) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.shift();
+        if (index === this.length) return !!this.pop();
 
         let nodeToRemove = this.get(index);
         let priorNode = nodeToRemove.prev;
@@ -162,10 +173,10 @@ class DoublyLinkedList {
         return true;
     }
 }
-dll = new DoublyLinkedList();
+const dll = new DoublyLinkedList();
 dll.push(1);
 dll.push(2);
-    //   5
+//   5
 dll.push(3);
 
 dll.push(4);
